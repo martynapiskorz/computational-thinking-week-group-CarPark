ce66ca1115f4020ad43c594396f6136cf6e036e3
majmurm <maja.murawka@gmail.com>
2023-09-04 15:03:58 +0200

    Update eryk.py

--- challenge_day1/eryk.py
+++ challenge_day1/eryk.py
@@ -4,4 +4,4 @@ def name_eryk():
 character_name = "Enrico"
 
 def paragraph1b():
-    print("After making myself comfortable and unpacking in my room I decided to head for the pool. It was so hot and I couldn’t imagine spending this evening without jumping into the water. I was walking around the pool, looking for a sunbed, when I passed the bar and saw a bery symphatetic smile, he introduced himself as Enrico. He was the hotel’s bartender. He looked visible hangover and explained that he was celebrating birthday yesterday.")
+    print("After making myself comfortable and unpacking in my room I decided to head for the pool. It was so hot and I couldn’t imagine spending this evening without jumping into the water. I was walking around the pool, looking for a sunbed, when I passed the bar and saw a bery symphatetic smile, he introduced himself as Enrico. He was the hotel’s bartender. He looked visibly hangover and explained that he was celebrating birthday yesterday.")
