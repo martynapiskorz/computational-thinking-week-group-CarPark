02448e2cf2907c1d336ea9cfe55d282cbcc6c96b
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-09-04 14:33:57 +0200

    story

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,9 +1,10 @@
 from maja import get_name as name1
-from martyna import string_name as name2
+from martyna import string_name as name2, paragraph1
 from Vincent import name as name3
 from eryk import name_eryk as name4
 
 def funct():
     print(f"This is CarPark. We are {name1()}, {name2()}, {name3()}, {name4()}")
-    
+    story = paragraph1()
+    return story
 funct()
