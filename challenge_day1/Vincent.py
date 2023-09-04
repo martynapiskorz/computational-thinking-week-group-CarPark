9ffa8f88d926fc77b2419cd7611ea0ba31f40a93
VincentUVA <vincent.hendriksen@student.uva.nl>
2023-09-04 15:06:10 +0200

    Update Vincent.py

--- challenge_day1/Vincent.py
+++ challenge_day1/Vincent.py
@@ -1,2 +1,7 @@
 def name():
     return "Vincent"
+
+'''character name: Jose'''
+
+def paragraph1_c():
+    print("I asked him who's birthday he celebrated. The young man excitedly told me about his chiuahua named Jose. I though his story was very boring, but the man was so cutge, that I wanted to meet his dog as well. Turns out, it was an annoyin and mean dog, so I left to never see Enrico or his dog ever again.")
