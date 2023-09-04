e222d1ee855c0b9a572b8eb5a19ad71c037a5d2b
matekoves <mate.koves@student.uva.nl>
2023-09-04 15:35:49 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,7 +2,7 @@ from maja import get_name as name1
 from martyna import string_name as name2, paragraph1, paragraph1e
 from Vincent import name as name3, paragraph1c
 from eryk import name_eryk as name4, paragraph1b
-from mate import get_name as name5
+from mate import get_myname as name5
 
 def funct():
     print(f"This is CarPark. We are {name1()}, {name2()}, {name3()}, {name4()}, {name5()}.\n")
