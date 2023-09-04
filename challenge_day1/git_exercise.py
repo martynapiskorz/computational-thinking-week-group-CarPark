71b4de815f9966c725f71408519a856ed4056e4a
ekyreso <110468195+ekyreso@users.noreply.github.com>
2023-09-04 14:28:37 +0200

    Hehe XD

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,9 @@
+from maja import get_name as name1
+from martyna import string_name as name2
+from Vincent import name as name3
+from eryk import name_eryk as name4
+
+def funct():
+    print(f"This is CarPark. We are {name1()}, {name2()}, {name3()}, {name4()}")
+    
+funct()
