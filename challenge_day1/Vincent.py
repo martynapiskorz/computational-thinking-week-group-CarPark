7b2ea3c360f286cd5b96b20bbbda3fb0d4204d63
ekyreso <110468195+ekyreso@users.noreply.github.com>
2023-09-04 15:11:00 +0200

    Updated the dog

--- challenge_day1/Vincent.py
+++ challenge_day1/Vincent.py
@@ -4,4 +4,4 @@ def name():
 '''character name: Jose'''
 
 def paragraph1_c():
-    print("I asked him who's birthday he celebrated. The young man excitedly told me about his chiuahua named Jose. I though his story was very boring, but the man was so cutge, that I wanted to meet his dog as well. Turns out, it was an annoyin and mean dog, so I left to never see Enrico or his dog ever again.")
+    print("I asked him who's birthday he celebrated. The young man excitedly told me about his chihuahua named Jose. I though his story was very boring, but the man was so cute, that I wanted to meet his dog as well. Turns out, it was an annoyin and mean dog, so I left to never see Enrico or his dog ever again.")
