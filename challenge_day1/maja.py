6afb0e3eee8b1e763518090c633076a025859ac7
majmurm <maja.murawka@gmail.com>
2023-09-04 15:29:21 +0200

    Update maja.py

--- challenge_day1/maja.py
+++ challenge_day1/maja.py
@@ -1,2 +1,7 @@
 def get_name():
     return "Maja"
+
+""""character name: Mauricio"""
+
+def paragraph1d():
+    print("I decided to head to the bitch. As it quickly turned out, I forgot the only thing I could possibly need - a sqimsuit.  It was missing from the suitcase. What if someone stole it? Could it be the housekeeper? It didn't matter. The only thing I cared about was the swimsuit - a beautiful, purple bikini. I decided to confront the front office assistant. A short conversation with Mauricio, a middle-aged man with a receding hairline, confirmed my suspicions - somewane entered my room after I checked in. Mauricio tried to convince me that there was no chance of his staff removing a guest belonging from a room, so talking to him did not make any sense. I just needed my bikini back.")
