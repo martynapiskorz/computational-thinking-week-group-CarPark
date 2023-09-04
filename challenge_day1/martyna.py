e502a3bbe37c8561e56137821abe76472952fa0b
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-09-04 15:25:42 +0200

    g

--- challenge_day1/martyna.py
+++ challenge_day1/martyna.py
@@ -7,7 +7,7 @@ def paragraph1():
     print("I always wanted to go to Brazil… This year my dream finally came true. I bought a plane ticket and packed my suitcase which was so huge and heavy that I could barely lift it. The flight was 7 hours but I slept for the most of it. Finally, I landed. When I got off the plane, hot and humid air hit me and it was hard to breathe. But the view in front of me made up for it. I thought to myself, “Okay Eleonora, maybe you suffocate here but at least you’ll be surrounded by beautiful nature”. Anyway, I taked a bus to my hotel which was so crowded that I was squeezed against the door the whole time. I arrived at the hotel in the late evening.")
     
 def paragraph1e():
-    print("Howewer, I couldn't let go the swimsuit theft so easily. I decided to startt an interrogation on my own. While I was going through the corrdidors, I heard that stupid bartender's voice. I saw him tallking to a young blonde (he called her 'Victoria' and then... I couldn't believe my eyes!!! There it was! My swimsuit sticking out of his back pocket.")
+    print("Howewer, I couldn't let go the swimsuit theft so easily. I decided to startt an interrogation on my own. While I was going through the corrdidors, I heard that stupid bartender's voice. I saw him tallking to a young boy (he called him 'Daniel') and then... I couldn't believe my eyes!!! There it was! My swimsuit sticking out of his back pocket.")
     
 def paragraph3():
     print("")
