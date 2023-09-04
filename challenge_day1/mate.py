d34a85cfca1e70999fe7a28cc6be958be3dee603
VincentUVA <vincent.hendriksen@student.uva.nl>
2023-09-04 15:49:50 +0200

    mate typos

--- challenge_day1/mate.py
+++ challenge_day1/mate.py
@@ -4,4 +4,4 @@ def get_myname():
 '''character name: Daniel'''
 
 def paragraph1e():
-    print("Daniel stoof there next tot that sttopid bartender with a smirk onn his face. Drinkink a long island ice tee talking to another chik. I could klearly see my swimsuit, there was non question about it! I went and grebbed a glass of vater, poured the whole thing in hes face and slpped this thief. Tokk my swimsuit from his pocked and ran upp to my room crying.")
+    print("Daniel stood there next to that stupid bartender with a smirk on his face. Drinking a long island ice tea talking to another chick. I could clearly see my swimsuit, there was no question about it! I went and grabbed a glass of water, poured the whole thing in his face and slapped this thief. Took my swimsuit from his pocket and ran up to my room crying.")
