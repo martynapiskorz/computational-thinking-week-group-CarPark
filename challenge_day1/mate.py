7fdd44727196cfcc9d3d19e12da5ce88125c2de9
matekoves <mate.koves@student.uva.nl>
2023-09-04 15:45:04 +0200

    Update mate.py

--- challenge_day1/mate.py
+++ challenge_day1/mate.py
@@ -4,4 +4,4 @@ def get_myname():
 '''character name: Daniel'''
 
 def paragraph2a():
-    print("")
+    print("Daniel stoof there next tot that sttopid bartender with a smirk onn his face. Drinkink a long island ice tee talking to another chik. I could klearly see my swimsuit, there was non question about it! I went and grebbed a glass of vater, poured the whole thing in hes face and slpped this thief. Tokk my swimsuit from his pocked and ran upp to my room crying.")
