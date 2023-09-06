481cdeb5b2fbe5a7de27f9a198372802e4b03526
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-09-06 14:06:09 +0200

    Update station1.py

--- challenge_day3/station1.py
+++ challenge_day3/station1.py
@@ -1,9 +1,5 @@
 def solution_station_1(n):
-    if n<0:
-        print("Incorrect input")
-    elif n==0:
-        return(0)
-    elif n==1:
-        return(1)
-    else:
-        return (solution_station_1(n-1)+solution_station_1(n-2))
+    x, y = 0, 1
+    for i in range(n):
+        x, y = y, x+y
+    return x
