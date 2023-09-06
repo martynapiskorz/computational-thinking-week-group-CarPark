06b30cd8e93fefda31fe647a8b0332a045ceb40b
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-09-06 11:30:45 +0200

    Create station7.py

---
+++ challenge_day3/station7.py
@@ -0,0 +1,11 @@
+def solution_station_7(expression):
+    variables = {'a': 3, 'b' : -1, 'c' : 4, 'd' : 7, 'e' : 0.5}
+   
+    for var, val in variables.items():
+        expression = expression.replace(var, str(val))
+
+    return eval(expression)
+
+solution_station_7('a+b')
+
+    
