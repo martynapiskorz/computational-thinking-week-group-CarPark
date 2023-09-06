48d51a3e30e14f8d9c4004ad6c90a5ad57d5f15e
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-09-06 13:11:42 +0200

    s

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -5,6 +5,9 @@ table_path = "challenge_day3/teamsnames.xlsx"
 df = pd.read_excel(table_path)
 
 def solution_station_5(name):
+    table_path = "challenge_day3/teamsnames.xlsx"
+    df = pd.read_excel(table_path)
+    
     try:
         team = df.loc[df['Student firstname'] == name, 'Team'].values[0]
         return team
