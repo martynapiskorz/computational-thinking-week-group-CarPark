924c5b38c99be2ca7759429ab08736d8a78a4b55
martynapiskorz <126684779+martynapiskorz@users.noreply.github.com>
2023-09-06 13:15:35 +0200

    s

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
-table_path = "challenge_day3/teamsnames.xlsx"
-
-df = pd.read_excel(table_path)
-
 def solution_station_5(name):
     table_path = "challenge_day3/teamsnames.xlsx"
     df = pd.read_excel(table_path)
