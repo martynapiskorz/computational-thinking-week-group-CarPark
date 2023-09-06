bb42a447ab00cabea62be2156090df6511310988
matekoves <mate.koves@student.uva.nl>
2023-09-06 11:31:12 +0200

    Update station4.py

---
+++ challenge_day3/station4.py
@@ -0,0 +1,10 @@
+def get_odd(n):
+    if n % 2 == 0:
+        return "False"
+    else:
+        return "True"
+    
+test_number = 7
+result = get_odd(test_number)
+
+print(f'{test_number} is {result}')
