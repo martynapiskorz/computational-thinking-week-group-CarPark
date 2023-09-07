065294aafb4f2f7bae5aee74ea85110a77f34295
VincentUVA <vincent.hendriksen@student.uva.nl>
2023-09-07 14:35:00 +0200

    Update Vincent.py

--- challenge_day1/Vincent.py
+++ challenge_day1/Vincent.py
@@ -4,6 +4,11 @@ def name():
 def character():
     return 'Jose'
 
-
 def paragraph1c(c1, c2, c3, c4, c5):
-    print(f"{c1}{c2}{c3}{c4}{c5}I asked him whose birthday he celebrated. The young man excitedly told me about his chiuahua named Jose. I though his story was very boring, but the man was so cute, that I wanted to meet his dog as well. Turns out, it was an annoying and mean dog, so I left to never see Enrico or his dog ever again.")
+    print(f"It was so hot and I couldn’t imagine spending this evening without jumping into the water. I ({c2}) was walking around the pool, looking for a sunbed, when I passed the bar and saw a bery symphatetic smile, he introduced himself as {c4}. He was the hotel’s bartender. He looked visibly hangover and explained that he was celebrating birthday yesterday. I asked him whose birthday he celebrated. The young man excitedly told me about his chiuahua named {c3} and briefly mentioned his friends, {c1} and {c5}. I though his story was very boring, but the man was so cute, that I wanted to meet his dog as well. Turns out, it was an annoying and mean dog, so I left to never see {c4} or his dog ever again.")
+
+def paragraph2c(c1, c2, c3, c4, c5):
+    print(f"The next morning, I ventured out for a peaceful stroll along the beach, trying to clear my head. Please, {c2}, you can't let this ruin your vacation. Unexpectedly, I crossed paths with {c3}, who was taking {c1}'s Chihuahua, {c5}, for a walk. We exchanged awkward greetings, and I couldn't help but wonder if {c4} was still nursing his bruised cheek from the previous night. {c3} assured me that he had no involvement in the swimsuit incident. I remained skeptical but chose to give him the benefit of the doubt.")
+
+def paragraph3c(c1, c2, c3, c4, c5):
+    print(f"As {c2}, {c1}, {c3}, {c4}, and {c5} continued spending time together, we discovered a shared appetite for adventure. We decided to embark on an audacious journey into the heart of the Amazon rainforest, facing thrills and perils that tested the bonds between {c1}, {c2}, {c3}, {c4}, and {c5}. Our dependence on each other to navigate the dense jungle deepened our friendships.")
