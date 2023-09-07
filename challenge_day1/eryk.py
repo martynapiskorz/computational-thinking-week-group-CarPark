e2a14197535eb890f0e7c736f5699c3b9fbfd5e0
ekyreso <110468195+ekyreso@users.noreply.github.com>
2023-09-07 14:12:54 +0200

    updated eryk.py

--- challenge_day1/eryk.py
+++ challenge_day1/eryk.py
@@ -5,4 +5,10 @@ def character():
     return 'Enrico'
 
 def paragraph1b(c1, c2, c3, c4, c5):
-    print(f"{c1}{c2}{c3}{c4}{c5}After making myself comfortable and unpacking in my room I decided to head for the pool. It was so hot and I couldn’t imagine spending this evening without jumping into the water. I was walking around the pool, looking for a sunbed, when I passed the bar and saw a bery symphatetic smile, he introduced himself as Enrico. He was the hotel’s bartender. He looked visibly hangover and explained that he was celebrating birthday yesterday.")
+    print(f"I thought to myself, “Okay {c2}, maybe you suffocate here but at least you’ll be surrounded by beautiful nature”. Anyway, I took a bus to my hotel which was so crowded that I was squeezed against the door the whole time. I arrived at the hotel in the late evening, not aware that I am yet to meet {c1},{c3},{c4}, and {c5}. After making myself comfortable and unpacking in my room I decided to head for the pool.")
+
+def paragraph2b(c1, c2, c3, c4, c5):
+    print(f"As I stormed back to my room, still fuming about the swimsuit incident, I couldn't help but think about the chaos caused by {c4}, {c5}, and even {c3}. The whole situation had me seething. {c2}, calm down. I decided to spend the evening watching Netflix, trying to calm my nerves, but curiosity got the best of me. I wondered what kind of mess {c1}, {c4}, and {c5} were stirring up downstairs.")
+
+def paragraph3b(c1, c2, c3, c4, c5):
+    print(f"One evening, as the sun dipped below the horizon, {c4} approached me with an apologetic grin. He admitted that he had been wrong to take my swimsuit and admitted that a bet with {c5} had spiraled out of control, leading to a chaotic situation involving {c1}, {c2}, {c3}, {c4}, and {c5}. We both laughed at the absurdity of it all and decided to let go of the past. {c4} even offered to prepare a special cocktail as a peace offering.")
