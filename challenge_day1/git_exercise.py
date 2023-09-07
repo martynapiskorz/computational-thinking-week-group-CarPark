f3bdd05d7a2598cb996c9670effdb8555c24114b
majmurm <maja.murawka@gmail.com>
2023-09-07 12:55:22 +0200

    :]

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,5 +1,3 @@
-
-
 import maja
 import martyna
 import Vincent
@@ -10,6 +8,6 @@ c1, c2, c3, c4, c5 = maja.character(), martyna.character(), Vincent.character(),
 
 def funct():
     print(f"This is CarPark. We are {maja.get_name()}, {martyna.string_name()}, {Vincent.name()}, {eryk.name_eryk()}, {mate.get_myname()}.\n")
-    story = martyna.paragraph1(c1, c2, c3, c4, c5), eryk.paragraph1b(c1, c2, c3, c4, c5), Vincent.paragraph1c(c1, c2, c3, c4, c5), maja.paragraph1d(c1, c2, c3, c4, c5), martyna.paragraph1e(c1, c2, c3, c4, c5), mate.paragraph2a(c1, c2, c3, c4, c5)
+    story = martyna.paragraph1(c1, c2, c3, c4, c5), eryk.paragraph1b(c1, c2, c3, c4, c5), Vincent.paragraph1c(c1, c2, c3, c4, c5), maja.paragraph1d(c1, c2, c3, c4, c5), martyna.paragraph1e(c1, c2, c3, c4, c5), mate.paragraph2a(c1, c2, c3, c4, c5), eryk.paragraph2b(c1, c2, c3, c4, c5), Vincent.paragraph2c(c1, c2, c3, c4, c5), maja.paragraph2d(c1, c2, c3, c4, c5), mate.paragraph2e(c1, c2, c3, c4, c5), martyna.paragraph3(c1, c2, c3, c4, c5), eryk.paragraph3b(c1, c2, c3, c4, c5), Vincent.paragraph3c(c1, c2, c3, c4, c5), maja.paragraph3d(c1, c2, c3, c4, c5), mate.paragraph3e(c1, c2, c3, c4, c5)
     return story
 funct()
--- challenge_day1/martyna.py
+++ challenge_day1/martyna.py
@@ -5,10 +5,10 @@ def character():
     return 'Eleonora'
 
 def paragraph1(c1, c2, c3, c4, c5):
-    print(f"{c1}{c2}{c3}{c4}{c5}I always wanted to go to Brazil… This year my dream finally came true. I bought a plane ticket and packed my suitcase which was so huge and heavy that I could barely lift it. The flight was 7 hours but I slept for the most of it. Finally, I landed. When I got off the plane, hot and humid air hit me and it was hard to breathe. But the view in front of me made up for it. I thought to myself, “Okay Eleonora, maybe you suffocate here but at least you’ll be surrounded by beautiful nature”. Anyway, I taked a bus to my hotel which was so crowded that I was squeezed against the door the whole time. I arrived at the hotel in the late evening.")
+    print(f"Dear diary, This is {c2}, once again. I keep thinking about my crazy adventure. All these people - {c1}, {c3}, {c4}, {c5}... It was so unexpected! But this is how it all started: I always wanted to go to Brazil… This year my dream finally came true. I bought a plane ticket and packed my suitcase which was so huge and heavy that I could barely lift it. The flight was 7 hours but I slept for the most of it. Finally, I landed. When I got off the plane, hot and humid air hit me and it was hard to breathe. But the view in front of me made up for it.")
     
 def paragraph1e(c1, c2, c3, c4, c5):
     print(f"Howewer, I couldn't let go the swimsuit theft so easily. My mum always says: '{c2}, you should never give up'. I decided to start an interrogation on my own. While I was going through the corrdidors, I saw this dog, {c3}, and I heard that bartender {c4}'s voice. I saw him tallking to a young boy (he called him {c5}) and {c1} and then... I couldn't believe my eyes!!! There it was! My swimsuit sticking out of his back pocket.")
     
 def paragraph3(c1, c2, c3, c4, c5):
-    print(f"{c1}{c2}{c3}{c4}{c5}")
+    print(f"As the days rolled on, tensions seemed to ease between {c2}, {c3}, and {c1}. We spent more time together exploring the captivating Brazilian landscapes, sharing stories, and even chuckling about the swimsuit debacle caused by {c4} and {c5}. I realized that sometimes misunderstandings can pave the way for unexpected friendships, and I felt grateful for the adventure that had drawn me closer to {c1}, {c3}, and even {c5}.")
